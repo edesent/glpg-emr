1d285f174df615e92886c6f704886baef89ffdea
good3n <good3n@gmail.com>
2020-11-04 12:43:59 -0500

    initial commit

---
+++ components/Footer/Footer.component.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="fixed bottom-0 inset-x-auto">
+    <a
+      href="https://greatlakespsychologygroup.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      &copy; Great Lakes Psychology Group
+    </a>
+  </footer>
+);
+
+export default Footer;
