5e347a34199aba29933204020997c9e4a309c90e
good3n <good3n@gmail.com>
2020-11-06 12:42:48 -0500

    temporarily remove amplify support

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import "../styles/globals.css";
 import "../styles/tailwind.css";
-import { Amplify } from "aws-amplify";
-import awsExports from "../aws-exports";
-Amplify.configure({ ...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
