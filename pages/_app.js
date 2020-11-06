b2492a4288bbd2c54722f68fb475feaa0cd3ec34
good3n <good3n@gmail.com>
2020-11-06 12:32:59 -0500

    amplify support

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "../styles/globals.css";
 import "../styles/tailwind.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "../aws-exports";
+Amplify.configure({ ...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
