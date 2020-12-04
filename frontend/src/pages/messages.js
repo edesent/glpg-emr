2a831d04610be30acbcfdea4225ee936130f28d9
Tom <good3n@gmail.com>
2020-12-04 13:37:47 -0500

    Restructure app to avoid importing components too many times

--- frontend/src/pages/messages.js
+++ frontend/src/pages/messages.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { Header } from '../components/Header'
 import { Submenu } from '../components/Submenu'
-import { Footer } from '../components/Footer'
 
 const MessagesPage = () => {
   return (
     <Layout title="Messages">
-      <Header />
       <Submenu title="Messages" />
-      CI / CD is a Go
-      <Footer />
     </Layout>
   )
 }
--- frontend/src/pages/patients.js
+++ frontend/src/pages/patients.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { Header } from '../components/Header'
 import { Submenu } from '../components/Submenu'
-import { Footer } from '../components/Footer'
 
 const PatientsPage = () => {
   return (
     <Layout title="GLPG Notes">
-      <Header />
       <Submenu title="Patients" />
-      <Footer />
     </Layout>
   )
 }
