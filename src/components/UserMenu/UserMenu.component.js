10eaabcbfe142032fadeff495ec3dd9d5b654b43
Tom <good3n@gmail.com>
2020-12-15 18:39:28 -0500

    Style user dropdown links

--- src/components/UserMenu/UserMenu.component.js
+++ src/components/UserMenu/UserMenu.component.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
-import { StyledUserMenu, Menu, Avatar, Dropdown } from './UserMenu.styles'
+import { Link } from 'react-router-dom'
+import {
+  StyledUserMenu,
+  Menu,
+  Avatar,
+  Dropdown,
+  StyledLogout,
+} from './UserMenu.styles'
 
 const UserMenu = () => {
   const [userMenuOpen, setUserMenuOpen] = useState(false)
@@ -24,7 +31,7 @@ const UserMenu = () => {
         </Avatar>
 
         <div className="role-name">
-          <span>Developer</span> Tom G.
+          <span>Role Title</span> Harry M.
         </div>
 
         <svg
@@ -41,7 +48,46 @@ const UserMenu = () => {
           />
         </svg>
       </Menu>
-      <Dropdown className={userMenuOpen ? `is-open` : ``}>Beep boop</Dropdown>
+      <Dropdown className={userMenuOpen ? `is-open` : ``}>
+        <Link to="/" onClick={(e) => e.preventDefault}>
+          <svg
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+            />
+            <path
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+            />
+          </svg>
+          <span>My Account</span>
+        </Link>
+        <StyledLogout>
+          <svg
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+            />
+          </svg>
+          <span>Logout</span>
+        </StyledLogout>
+      </Dropdown>
     </StyledUserMenu>
   )
 }
--- src/components/UserMenu/UserMenu.styles.js
+++ src/components/UserMenu/UserMenu.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Logout from '../Logout'
 
 export const StyledUserMenu = styled.div`
   position: relative;
@@ -42,16 +43,54 @@ export const Dropdown = styled.div`
   position: absolute;
   top: calc(100% - 20px);
   right: 0;
-  background-color: #f4f7ff;
+  background-color: #fff;
   border-radius: 10px;
   padding: 30px;
   visibility: hidden;
   opacity: 0;
   transition: all 0.15s linear;
+  box-shadow: 0 0 25px -5px rgba(0, 0, 0, 0.15),
+    0 10px 10px -5px rgba(0, 0, 0, 0.04);
 
   &.is-open {
     top: 100%;
     visibility: visible;
     opacity: 1;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    font-weight: 500;
+    margin-bottom: 20px;
+
+    svg {
+      width: 18px;
+      margin-right: 10px;
+
+      path {
+        stroke: #0f5ef7;
+      }
+    }
+  }
+`
+
+export const StyledLogout = styled(Logout)`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  font-weight: 500;
+  background: none;
+  padding: 0;
+  outline: 0;
+
+  svg {
+    width: 18px;
+    margin-right: 10px;
+
+    path {
+      stroke: #0f5ef7;
+    }
   }
 `
