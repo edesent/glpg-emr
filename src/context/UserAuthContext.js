d46e602a2823abc0987edb37dd32343ec669b2c9
Elijah Desent <elimark1611@gmail>
2021-01-06 12:57:07 -0500

    Turn what was in app.js to a wrapper

--- src/context/UserAuthContext.js
+++ src/context/UserAuthContext.js
@@ -6,24 +6,35 @@ import { useRealmApp } from './RealmContext'
 const UserAuthContext = createContext()
 
 export const UserAuth = ({ children }) => {
-    // We need to have a loading indicator
-    const [userAuthenticating, setUserAuthenticating] = useState(true)
-    // We need to bring in realmApp
-    const app = useRealmApp()
-    // We need a function that triggers on pageload then ends on reply from RealmApp
-    async function onLoad() {
-      try {
-          await app.currentUser
-          if (app?.currentUser) { 
-            setUserAuthenticated(true)
-          }
-        // console.log(app.currentUser)
-      } catch (error) {
-        if (error) {
-          // eslint-disable-next-line no-alert
-          setUserAuthenticated(false)
-          alert(error)
-        }
+  // We need to have a loading indicator
+  const [userAuthenticating, setUserAuthenticating] = useState(true)
+  // We need to bring in realmApp
+  const app = useRealmApp()
+  // We need a function that triggers on pageload then ends on reply from RealmApp Copied from Tom
+  async function onLoad() {
+    try {
+      await app.currentUser
+      app?.currentUser && setUserAuthenticated(true)
+      // console.log(app.currentUser)
+    } catch (error) {
+      if (error) {
+        // eslint-disable-next-line no-alert
+        setUserAuthenticated(false)
+        alert(error)
       }
-      setUserAuthenticating(false)
     }
+    setUserAuthenticating(false)
+  }
+  useEffect(() => {
+    onLoad()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <UserAuthContext.Provider value={userAuthenticating}>
+      {!userAuthenticating && children}
+    </UserAuthContext.Provider>
+  )
+}
+UserAuth.propTypes = {
+  children: PropTypes.node,
+}
