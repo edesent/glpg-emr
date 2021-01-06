800d5961ecc49b14b55c68e4553ba667b7b711ac
Elijah Desent <elimark1611@gmail>
2021-01-06 16:11:52 -0500

    Update routes to access Authenticated state through app

--- src/routes/AuthenticatedRoute.js
+++ src/routes/AuthenticatedRoute.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Route, Redirect, useLocation } from 'react-router-dom'
-import { useAppContext } from '../context/AppContext'
+import { useRealmApp } from '../context/RealmContext'
 
 export default function AuthenticatedRoute({ children, ...rest }) {
   const { pathname, search } = useLocation()
-  const { userAuthenticated } = useAppContext()
+  // We need to access realm app
+  const app = useRealmApp()
   return (
     <Route {...rest}>
-      {userAuthenticated ? (
+      {app.userAuthenticated ? (
         children
       ) : (
         <Redirect to={`/?redirect=${pathname}${search}`} />
