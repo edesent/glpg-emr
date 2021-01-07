2b6ffe244e572c00eea88ed17007a1ae57098137
Elijah Desent <elimark1611@gmail>
2021-01-06 21:08:46 -0500

    Simplify the create user mutation not worrying about apollo cache right now

--- src/graphql/createUser.js
+++ src/graphql/createUser.js
@@ -1,54 +1,38 @@
-import { ObjectId } from 'bson'
 import { gql, useMutation } from '@apollo/client'
-
-function createUserData(user) {
-  const CreateUserMutation = gql``
-
-  const userFieldsFragment = gql`
-    fragment TaskFields on Task {
-      _id
-      _partition
-      status
-      name
+// We need a wrapper for useMutation
+function createUserMutation(user) {
+  // The actual payload should be user
+  const createUserqgl = gql`
+    mutation createUser($user: UserInsertInput!) {
+      createdUser: insertOneAuthorizationUser(data: $user) {
+        FirstName
+        LastName
+        Email
+        _id
+      }
     }
   `
-  const [createUserMutation] = useMutation(CreateUserMutation, {
-    // We need to update the cache with the created user
-    update: (cache, { data: { createdUser } }) => {
-      cache.modify({
-        fields: {
-          tasks: (existingTasks = []) => [
-            ...existingTasks,
-            cache.writeFragment({
-              data: createdUser,
-              fragment: userFieldsFragment,
-            }),
-          ],
-        },
-      })
-    },
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { data, loading, error } = useMutation(createUserqgl, {
+    variables: { user },
   })
-  // We need a Mutation to create a user based off the userObject
-  const createUser = async (task) => {
-    const { createdUser } = await createUserMutation({
-      variables: {
-        task: {
-          _id: new ObjectId(),
-          _partition: user.partition,
-          status: 'Open',
-          ...task,
-        },
-      },
-    })
-    return createdUser
+  if (error) {
+    throw new Error(`Failed to create user. ${error.message}`)
   }
-  return createUser
+  const resposeData = data ?? []
+  return {
+    resposeData,
+    loading,
+  }
+}
+const createUser = async (user) => {
+  const { userResponse, loading, error } = await createUserMutation(user)
+
+  return {
+    loading,
+    userResponse,
+    error,
+  }
 }
 
-export default function userMutations(user) {
-  return {
-    createUser: createUserData(user),
-    // updateUser updateUserData(user),
-    // deleteUser: deleteUserData(user),
-  }
-}
+export default createUser
