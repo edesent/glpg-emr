7ba40ae2ae59fd06db6ed0ed5294bb95b8007f47
Tom Gooden <good3n@gmail.com>
2021-01-10 14:04:50 -0500

    Separate login form logic from markup via container

--- src/components/Login/Login.component.js
+++ src/components/Login/Login.component.js
@@ -1,65 +1,14 @@
-import React from 'react'
-import * as Realm from 'realm-web'
-import { useForm } from 'react-hook-form'
-import { useHistory } from 'react-router-dom'
-import { useRealmApp } from '../../context/RealmContext'
+import PropTypes from 'prop-types'
+import { Input } from '../Forms/Input'
 import { Error, Links } from './Login.styles'
 
-const Login = () => {
-  // hooks
-  const app = useRealmApp()
-  const { register, handleSubmit, errors } = useForm()
-  const history = useHistory()
-
-  async function userLogin(email, password) {
-    const credentials = Realm.Credentials.emailPassword(email, password)
-    try {
-      const user = await app.logIn(credentials)
-      if (user) {
-        return user
-      }
-      return null
-    } catch (error) {
-      // eslint-disable-next-line no-alert
-      alert('Invalid Login Credientials')
-      return null
-    }
-  }
-
-  const onSubmit = ({ email, password }) => {
-    userLogin(email, password).then((user) => {
-      if (user && user !== null) {
-        history.push('/dashboard')
-      }
-    })
-  }
-
+const LoginComponent = ({ errors, onSubmit, register }) => {
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={onSubmit}>
       <h1>Sign in to your account</h1>
-      {Object.entries(errors).length !== 0 && (
-        <Error>All fields are required!</Error>
-      )}
-      <div>
-        <input
-          id="email"
-          name="email"
-          placeholder=" "
-          ref={register({ required: true })}
-          type="email"
-        />
-        <label htmlFor="email">Email</label>
-      </div>
-      <div>
-        <input
-          id="password"
-          name="password"
-          placeholder=" "
-          ref={register({ required: true })}
-          type="password"
-        />
-        <label htmlFor="password">Password</label>
-      </div>
+      {errors.length !== 0 && <Error>All fields are required!</Error>}
+      <Input label="Email" name="email" ref={register} type="email" />
+      <Input label="Password" name="password" ref={register} type="password" />
       <Links>
         <a href="/?forgotPassword">Forgot password?</a>
         <button>Login</button>
@@ -68,4 +17,10 @@ const Login = () => {
   )
 }
 
-export default Login
+LoginComponent.propTypes = {
+  errors: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  register: PropTypes.string.isRequired,
+}
+
+export default LoginComponent
