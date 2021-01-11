b4ba277c01fb9fdf9feb4e2530887713e888338b
Elijah Desent <elimark1611@gmail>
2021-01-10 23:11:21 -0500

    Finish appContext to wrap a few states this should be expanded

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,7 +1,45 @@
-import { useContext, createContext } from 'react'
+import { useContext, createContext, useState } from 'react'
+import PropTypes from 'prop-types'
+import { useRealmApp } from './RealmContext'
+import readUser from '../graphql/readUser'
 
-export const AppContext = createContext(null)
+// We need a contexts for App Settings
+const AppSettingsContext = createContext()
 
-export function useAppContext() {
-  return useContext(AppContext)
+// We create a hook to be able to use app settings in other files
+export const useSettingsApp = () => {
+  const appSettings = useContext(AppSettingsContext)
+  if (!appSettings) {
+    throw new Error(
+      `You must use AppSettings inside of a <AppSettingsContext.Provider  />`
+    )
+  }
+  return appSettings
+}
+// This can be expanded but I threw in some details
+export const UseAppSettings = ({ children }) => {
+  const [appSettings, setAppSettings] = useState({ timeout: 120 })
+  const app = useRealmApp()
+
+  const [graphqlLoading, setGraphqlLoading] = useState(false)
+  const [userDetails, setUserDetails] = useState(
+    readUser(app.currentUser.profile.email)
+  )
+
+  const wrapper = {
+    ...appSettings,
+    setAppSettings,
+    graphqlLoading,
+    setGraphqlLoading,
+    userDetails,
+    setUserDetails,
+  }
+  return (
+    <AppSettingsContext.Provider value={wrapper}>
+      {children}
+    </AppSettingsContext.Provider>
+  )
+}
+UseAppSettings.propTypes = {
+  children: PropTypes.node,
 }
