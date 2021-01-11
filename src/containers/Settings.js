a577cf65c381c0e3b41c7eb548ae0e1ba8e1c5be
Elijah Desent <elimark1611@gmail>
2021-01-10 23:54:13 -0500

    Update settings page

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { Submenu } from '../components/Submenu'
+import { CreateUserForm } from '../components/CreateUserForm'
 
 const SettingsPage = () => {
   return (
     <Layout>
       <Submenu title="Settings" />
+      <CreateUserForm />
     </Layout>
   )
 }
