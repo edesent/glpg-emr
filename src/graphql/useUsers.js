efaad17e1983bc3910d6d83c2c6dbdb9da78800b
Elijah Desent <elimark1611@gmail>
2021-01-11 00:03:28 -0500

    Add framework for mutation class

---
+++ src/graphql/useUsers.js
@@ -0,0 +1,10 @@
+import useUserMutations from './useUserMutations'
+
+const useUser = (data) => {
+  const { addUser, updateUser } = useUserMutations(data)
+  return {
+    updateUser,
+    addUser,
+  }
+}
+export default useUser
