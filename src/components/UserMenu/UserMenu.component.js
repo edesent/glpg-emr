969ff056aac965821471b617b745200ba0f1a2f5
Tom <good3n@gmail.com>
2021-01-14 10:18:55 -0500

    Add heroicons and replace svgs

--- src/components/UserMenu/UserMenu.component.js
+++ src/components/UserMenu/UserMenu.component.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import {
+  UserOutline,
+  ChevronDown,
+  CogOutline,
+  LogoutOutline,
+} from 'heroicons-react'
 import useReadUser from '../../graphql/useReadUser'
 import { useRealmApp } from '../../context/RealmContext'
 import {
@@ -26,15 +32,7 @@ const UserMenu = () => {
       >
         <Avatar>
           {/* 👇 TODO: This SVG will be a fallback for users that do not have specified avatars */}
-          <svg
-            fill="none"
-            stroke="#fff"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs />
-            <path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zm-4 7a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-          </svg>
+          <UserOutline size="32" />
         </Avatar>
 
         <div className="role-name">
@@ -43,57 +41,15 @@ const UserMenu = () => {
           {user.readUser.authorizationUser.LastName}
         </div>
 
-        <svg
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M19 9l-7 7-7-7"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-          />
-        </svg>
+        <ChevronDown size="12" />
       </Menu>
       <Dropdown className={userMenuOpen ? `is-open` : ``}>
         <Link onClick={(e) => e.preventDefault} to="/">
-          <svg
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-            />
-            <path
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-            />
-          </svg>
+          <CogOutline size="18" />
           <span>My Account</span>
         </Link>
         <StyledLogout>
-          <svg
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-            />
-          </svg>
+          <LogoutOutline size="18" />
           <span>Logout</span>
         </StyledLogout>
       </Dropdown>
