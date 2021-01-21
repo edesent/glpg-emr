614fada75f02b70094dae829b6d2fb90ab799c1f
Tom <good3n@gmail.com>
2021-01-21 16:40:22 -0500

    Create searchbar and add to header

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,10 +1,14 @@
+import { SearchBar } from '../SearchBar'
 import { UserMenu } from '../UserMenu'
-import { StyledHeader } from './Header.styles'
+import { StyledHeader, GridTop } from './Header.styles'
 
 // eslint-disable-next-line react/prop-types
 const Header = ({ title, children }) => (
   <StyledHeader>
-    <UserMenu />
+    <GridTop>
+      <SearchBar />
+      <UserMenu />
+    </GridTop>
     <h1>{title}</h1>
     {children}
   </StyledHeader>
---
+++ src/components/SearchBar/SearchBar.component.js
@@ -0,0 +1,9 @@
+import { StyledSearch } from './SearchBar.styles'
+
+const SearchBar = () => (
+  <StyledSearch>
+    <input placeholder="Search patients" type="text" />
+  </StyledSearch>
+)
+
+export default SearchBar
