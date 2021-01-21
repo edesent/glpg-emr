ced72b2099608502f6b3f7d76f653dbd35c89e82
Tom <good3n@gmail.com>
2021-01-21 18:27:10 -0500

    Clean up routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -14,21 +14,40 @@ import AuthenticatedRoute from './AuthenticatedRoute'
 export default function Routes() {
   return (
     <Switch>
+      {/* login */}
       <UnauthenticatedRoute component={LoginPage} exact path="/" />
+
+      {/* dashboard */}
       <AuthenticatedRoute component={DashboardPage} exact path="/dashboard" />
+      <AuthenticatedRoute
+        component={DashboardPage}
+        path="/dashboard/:subLink"
+      />
+
+      {/* patients */}
       <AuthenticatedRoute component={PatientsPage} exact path="/patients" />
+
+      {/* schedule */}
       <AuthenticatedRoute component={SchedulePage} exact path="/schedule" />
+
+      {/* messages */}
+      <AuthenticatedRoute component={MessagesPage} exact path="/messages" />
+
+      {/* account */}
       <AuthenticatedRoute component={AccountPage} exact path="/account" />
       <AuthenticatedRoute
         component={AccountPage}
         path="/account/:job/:identifier?"
       />
+
+      {/* admin */}
       <AuthenticatedRoute component={AdminPage} exact path="/admin" />
       <AuthenticatedRoute
         component={AdminPage}
         path="/admin/:job/:identifier?"
       />
-      <AuthenticatedRoute component={MessagesPage} exact path="/messages" />
+
+      {/* 404 */}
       <Route component={NotFound} />
     </Switch>
   )
