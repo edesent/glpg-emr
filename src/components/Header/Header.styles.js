f33c1bcfea6d967598df4aad7ff5fa605f75db06
Tom <good3n@gmail.com>
2021-01-21 18:28:36 -0500

    Style header component

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components'
 
 export const StyledHeader = styled.header`
+  padding: 30px 80px 0;
   background-color: var(--color__gray--light);
+
+  h1 {
+    margin: 0;
+  }
+`
+
+export const GridTop = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  align-items: center;
 `
