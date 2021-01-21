a665e30163dd831d53c18226915793d38f9c5bda
Tom <good3n@gmail.com>
2021-01-21 13:46:04 -0500

    Remove createuser component from account container

--- src/containers/Account.js
+++ src/containers/Account.js
@@ -3,7 +3,6 @@ import { NavLink } from 'react-router-dom'
 import Layout from '../components/layout'
 import { SubNav } from '../components/Nav/SubNav'
 import { UpdateUserForm } from '../components/Forms/UpdateUser'
-import { CreateUserForm } from '../components/Forms/CreateUser'
 import { ManageAccountsForm } from '../components/Forms/ManageAccounts'
 import ResetPasswordLink from '../components/ResetPasswordLink/ResetPasswordLink.component'
 
@@ -22,9 +21,6 @@ const AccountPage = ({ match }) => {
           Activity
         </NavLink>
         <ResetPasswordLink />
-        <NavLink title="My Account" to={`${path}/createuser`}>
-          Create User
-        </NavLink>
       </SubNav>
       <div className="container-wrapper">
         {match.path === path ? (
@@ -34,7 +30,6 @@ const AccountPage = ({ match }) => {
             {
               {
                 updateuser: <UpdateUserForm match={match} />,
-                createuser: <CreateUserForm match={match} />,
               }[match?.params?.job]
             }
           </>
