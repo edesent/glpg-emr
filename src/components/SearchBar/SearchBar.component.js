96746bedc87dd995b1f9716e499947cd987ca74b
Tom <good3n@gmail.com>
2021-01-22 03:08:28 -0500

    Style search component

--- src/components/SearchBar/SearchBar.component.js
+++ src/components/SearchBar/SearchBar.component.js
@@ -2,6 +2,19 @@ import { StyledSearch } from './SearchBar.styles'
 
 const SearchBar = () => (
   <StyledSearch>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+      />
+    </svg>
     <input placeholder="Search patients" type="text" />
   </StyledSearch>
 )
--- src/components/SearchBar/SearchBar.styles.js
+++ src/components/SearchBar/SearchBar.styles.js
@@ -1,7 +1,34 @@
 import styled from 'styled-components'
 
 export const StyledSearch = styled.div`
+  position: relative;
+  background-color: #fff;
+  border-radius: 5px;
+  height: 40px;
+
+  svg {
+    position: absolute;
+    top: 11px;
+    left: 15px;
+    width: 18px;
+
+    path {
+      stroke: #829ab1;
+    }
+  }
+
   input {
     width: 100%;
+    border-radius: 5px;
+    padding: 0 20px 0 45px;
+    outline: 0;
+    height: 40px;
+
+    &::placeholder {
+      font-size: 14px;
+      position: relative;
+      top: -1px;
+      color: #829ab1;
+    }
   }
 `
